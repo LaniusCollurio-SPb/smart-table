eb4e4edafb01dd1de28660504a5276b6c955138c
Lanius Collurio <Lanius.Collurio@yandex.ru>
2026-05-25 17:31:32 +0300

    Feat(*.js): work is done

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,36 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
+// TODO: DONE #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+    // TODO: DONE #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                                   // Получаем ключи из объекта
+        .forEach((elementName) => {                                        // Перебираем по именам
+            elements[elementName].append(                                  // в каждый элемент добавляем опции
+                ...Object.values(indexes[elementName])                     // формируем массив имён, значений опций
+                    .map(name => {                                         // используйте name как значение и текстовое содержимое
+                        const option = document.createElement('option');   // создаем тег опции
+                        option.value = name;                               // присваиваем name в value тега option
+                        option.textContent = name;                         // присваиваем name в техтовое содержимое тега option
+                        return option;                                     // ввозвращаем тег опции
+                    })
+            )
+        });
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        // TODO: DONE #4.2 — обработать очистку поля
+        const clearButton = document.querySelectorAll('button[name=clear]');
+        Array.from(clearButton).forEach((elem) => {
+            const filterInput = elem.parentElement.querySelector('input');
+            if (action && action.name === 'clear') {
+                filterInput.value = '';
+                state['customer'] = '';
+                state['date'] = '';
+            }
+        });
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        // TODO: DONE #4.5 — отфильтровать данные используя компаратор
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -11,13 +11,47 @@ export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    // TODO: #1.2 DONE —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(templateId => {                  // перебираем нужный массив идентификаторов
+        root[templateId] = cloneTemplate(templateId);         // клонируем и получаем объект, сохраняем в таблице
+        root.container.prepend(root[templateId].container);   // добавляем к таблице после (append) или до (prepend)
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    after.forEach(templateId => {                             // перебираем нужный массив идентификаторов
+        root[templateId] = cloneTemplate(templateId);         // клонируем и получаем объект, сохраняем в таблице
+        root.container.append(root[templateId].container);    // добавляем к таблице после (append) или до (prepend)
+    });
+
+    // TODO: #1.3 DONE —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', (e) => {
+        onAction();
+    })
+
+    root.container.addEventListener('reset', (e) => {
+        setTimeout(onAction);
+    })
+
+    root.container.addEventListener('submit', (e) => {
+        console.log(e.submitter)
+        e.preventDefault();
+        onAction(e.submitter);
+    })
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        // TODO: #1.1 DONE — преобразовать данные в массив строк на основе шаблона rowTemplate
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    if (row.elements.tagName === 'INPUT' || row.elements.tagName === 'SELECT') {
+                        row.elements[key].value = item[key];
+                    } else {
+                        row.elements[key].textContent = item[key];
+                    }
+                }
+            })
+            return row.container;
+        })
         root.elements.rows.replaceChildren(...nextRows);
     }
 
